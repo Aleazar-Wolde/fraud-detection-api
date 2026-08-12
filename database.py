8b191e0a37f6da350ccd9e027ae4b780a2c0f710
Aleazar Wolde <aleazarali123@gmail.com>
2026-08-12 16:04:50 -0400

    refactor: conver database connection into reusable get_connection

--- database.py
+++ database.py
@@ -9,10 +9,10 @@ database=os.getenv("DB_NAME")
 user=os.getenv("DB_USER")
 password=os.getenv("DB_PASSWORD")
 
-connection = psycopg2.connect(
-    host=host,
-    database=database,
-    user=user,
-    password=password
-)
-print("Connected to PostgreSQL successfully")
+def get_connection():
+    return psycopg2.connect(
+        host=host,
+        database=database,  
+        user=user,
+        password=password
+    )
