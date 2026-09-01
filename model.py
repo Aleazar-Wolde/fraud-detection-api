24ce55ee12481a5296848b6108b0ef027e2e56b9
Aleazar Wolde <aleazarali123@gmail.com>
2026-09-01 13:07:40 -0400

    Add model evaluation metrics and prediction probabilities

---
+++ model.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score
+
+df = pd.read_csv("data/archive/creditcard.csv") 
+X = df.drop("Class", axis=1)
+Y = df["Class"]
+model = LogisticRegression()
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X,
+    Y,
+    test_size=0.2,
+    random_state=42,
+    stratify=Y
+) 
+model.fit(X_train, Y_train)
+predictions  = model.predict(X_test)
+
+print(predictions[:10])
+print(Y_test.iloc[:10].to_numpy())
+# print(model.coef_)
+# print(model.intercept_)
+
+cm = confusion_matrix(Y_test, predictions)
+print(cm)
+
+precision = precision_score(Y_test, predictions)
+recall = recall_score(Y_test, predictions)
+print("Precision:", precision)
+print("Recall:", recall)
+
+probabilities = model.predict_proba(X_test)
+print(probabilities[:5])
