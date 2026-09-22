1097f892f4da51e5af2ed48d2f57ee7aa119fb7c
Aleazar Wolde <aleazarali123@gmail.com>
2026-09-22 16:20:46 -0400

    Add feature scaling and retune fraud threshold

--- model.py
+++ model.py
@@ -3,11 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score, recall_score
+from sklearn.preprocessing import StandardScaler 
 
 df = pd.read_csv("data/archive/creditcard.csv") 
 X = df.drop("Class", axis=1)
 Y = df["Class"]
-model = LogisticRegression()
+model = LogisticRegression(max_iter=1000)
+scaler = StandardScaler()
 
 X_train, X_temp, Y_train, Y_temp, = train_test_split(
     X,
@@ -25,8 +27,13 @@ X_validation, X_test, Y_validation, Y_test = train_test_split(
     stratify= Y_temp
 )
 
-model.fit(X_train, Y_train)
-predictions  = model.predict(X_validation)
+X_train_scaled = scaler.fit_transform(X_train)
+X_validation_scaled = scaler.transform(X_validation)
+X_test_scaled= scaler.transform(X_test)
+
+
+model.fit(X_train_scaled, Y_train)
+predictions  = model.predict(X_validation_scaled)
 
 print(predictions[:10])
 print(Y_validation.iloc[:10].to_numpy())
@@ -41,7 +48,7 @@ recall = recall_score(Y_validation, predictions)
 print("Precision:", precision)
 print("Recall:", recall)
 
-probabilities = model.predict_proba(X_validation)
+probabilities = model.predict_proba(X_validation_scaled)
 print(probabilities[:5])
 
 fraud_probabilities = probabilities[:,1]
@@ -76,18 +83,18 @@ print(Y_validation.value_counts())
 print(Y_test.value_counts())
 
 
-new_custom_probabilities= model.predict_proba(X_test)
+new_custom_probabilities= model.predict_proba(X_test_scaled)
 test_fraud_probabilities = new_custom_probabilities[:, 1]
 
-thresholds =0.9
+threshold =0.1
 
-custom_predictions = (test_fraud_probabilities >= thresholds).astype(int)
+custom_predictions = (test_fraud_probabilities >= threshold).astype(int)
 custom_cm = confusion_matrix(Y_test, custom_predictions)
 custom_precision = precision_score(Y_test, custom_predictions)
 custom_recall = recall_score(Y_test, custom_predictions)
 
 print("Final Test Results")
-print("Threshold:", thresholds)
+print("Threshold:", threshold)
 print(custom_cm)
 print("Precision:", custom_precision)
 print("Recall:", custom_recall)
